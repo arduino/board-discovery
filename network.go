84d901aa1897f9983589b29d6aa81eca3774dced
Alessandro Sanino <a.sanino@bcmi-labs.cc>
2017-08-25 14:56:47 +0200

    fixing header arduino-create-agent -> board-discovery

--- network.go
+++ network.go
@@ -1,7 +1,7 @@
 /*
- * This file is part of arduino-create-agent.
+ * This file is part of board-discovery.
  *
- * arduino-create-agent is free software; you can redistribute it and/or modify
+ * board-discovery is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
